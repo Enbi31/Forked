a57d4ddebafd3c087b47718293f23282595f1a71
AdiProCoder000 <aditya.maini44@gmail.com>
2026-07-22 14:33:23 +0530

    rewrite HeroSection with Fork branding and search bar

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,8 +2,8 @@ import HeroSection from '@/components/landing/herosection';
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
+    <main className="min-h-screen bg-zinc-950">
+      <HeroSection onSearch={() => {}} />
     </main>
   );
 }
