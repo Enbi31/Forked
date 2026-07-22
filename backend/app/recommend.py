9f27198304d6f7a00b9e17afe03577cf6fde1ea1
Enbi31 <enbi1331@gmail.com>
2026-07-22 15:56:47 +0530

    modified recommend

--- backend/app/recommend.py
+++ backend/app/recommend.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 from schema import RecommendationRequest
+from ai_service import get_ai_recommendation
 
 router = APIRouter()
 
 @router.post("/recommend")
 def recommend(data:RecommendationRequest):
-    return{
-        "message": "Got request",
-        "data": data
-    }
+    return get_ai_recommendation(data.model_dump())
